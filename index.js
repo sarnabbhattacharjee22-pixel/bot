94a70fbca67ae906344ae31ef7a4fe126e58e34a
Sarnab Bhattacharjee <bansh8561@gmail.com>
2026-09-14 18:20:43 +0000

    Fix automated commit bot

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const jsonfile = require("jsonfile");
 const moment = require("moment");
+const random = require("random");
 const simpleGit = require("simple-git");
 const path = "./data.js";
-const date = moment().format();
-const data = {
-    date: date,
+const git = simpleGit();
+
+const markCommit = (weeks, days, remaining) => {
+    const date = moment()
+        .subtract(1, "y")
+        .add(1, "d")
+        .add(weeks, "w")
+        .add(days, "d")
+        .format();
+    const data = { date };
+
+    jsonfile.writeFile(path, data, (error) => {
+        if (error) throw error;
+
+        git.add([path]).commit(date, { "--date": date }, (commitError) => {
+            if (commitError) throw commitError;
+            makeCommits(remaining - 1);
+        });
+    });
 };
 
-jsonfile.writeFile(path, data);
+const makeCommits = (remaining) => {
+    if (remaining === 0) return git.push();
 
-simpleGit().add([path]).commit(date, { "--date": date }).push();
+    markCommit(random.default.int(0, 54), random.default.int(0, 6), remaining);
+};
+
+makeCommits(100);
