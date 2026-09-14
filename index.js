e5a47a5c12a7006d5ff4b7234fe92ff09f32f405
Sarnab Bhattacharjee <bansh8561@gmail.com>
2026-09-15 00:00:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git");
 const path = "./data.js";
-const date = moment().format();
+const date = moment().subtract(5, 'd').format();
 const data = {
     date: date,
 };
 
-jsonfile.writeFile(path, data);
+jsonfile.writeFile(path, data,()=>{
+simpleGit().add([path]).commit(date, { "--date": date }).push();    
+});
 
-simpleGit().add([path]).commit(date, { "--date": date }).push();
