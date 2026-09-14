743ddbefeeb25e5cda22163565a9d86e4131f877
Sarnab Bhattacharjee <bansh8561@gmail.com>
2026-09-15 00:05:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const moment = require("moment");
 const simpleGit = require("simple-git");
 const path = "./data.js";
 const date = moment().subtract(5, 'd').format();
-const data = {
-    date: date,
+const markCommit = (2, 3) => {
+    const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+    const data = {
+        date: date,
+    }
 };
 
 jsonfile.writeFile(path, data,()=>{
