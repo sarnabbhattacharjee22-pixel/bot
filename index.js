6ab739c532269f505d9cd76b2b7ced785d062d4b
Sarnab Bhattacharjee <246118040+sarnabbhattacharjee22-pixel@users.noreply.github.com>
2026-09-15 02:33:50 +0600

    Use linked GitHub commit email

--- index.js
+++ index.js
@@ -3,26 +3,32 @@ const moment = require("moment");
 const simpleGit = require("simple-git");
 const path = "./data.js";
 const git = simpleGit();
+const githubEmail = "246118040+sarnabbhattacharjee22-pixel@users.noreply.github.com";
 
-const makeCommits = async (n) => {
-    if (n === 0) {
-        await git.push("origin", "main", {"--set-upstream": null});
-        return;
+const makeCommits = async () => {
+    await git.addConfig("user.email", githubEmail, false, "local");
+    const startDate = moment("2025-01-01");
+    const endDate = moment("2026-01-01");
+    const commitsPerDay = 20;
+
+    for (const date = startDate.clone(); date.isBefore(endDate); date.add(1, "day")) {
+        for (let commitNumber = 1; commitNumber <= commitsPerDay; commitNumber += 1) {
+            const commitDate = date.format();
+            const data = { date: commitDate, commitNumber };
+
+            console.log(`${commitDate} (${commitNumber}/${commitsPerDay})`);
+            await new Promise((resolve, reject) => {
+                jsonfile.writeFile(path, data, (error) => {
+                    if (error) reject(error);
+                    else resolve();
+                });
+            });
+
+            await git.add([path]).commit("Update data", {"--date": commitDate});
+        }
     }
 
-    const date = moment().subtract(n, "days").startOf("day").format();
-    const data = { date };
-
-    console.log(date);
-    await new Promise((resolve, reject) => {
-        jsonfile.writeFile(path, data, (error) => {
-            if (error) reject(error);
-            else resolve();
-        });
-    });
-
-    await git.add([path]).commit("Update data", {"--date": date});
-    await makeCommits(n - 1);
+    await git.push("origin", "main");
 };
 
-makeCommits(365).catch(console.error);
+makeCommits().catch(console.error);
